d270fcadb45cc9058d0ce5f5161a9875bdd404fb
g0325076_Kon <g0325076@iwate-u.ac.jp>
2025-12-09 23:16:22 +0900

    修正

--- toilet-app/src/app/components/ui/calendar.tsx
+++ toilet-app/src/app/components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker@8.10.1";
+import { DayPicker } from "react-day-picker";
 
 import { cn } from "./utils";
 import { buttonVariants } from "./button";
@@ -60,11 +60,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        Chevron: ({ className, ...props }) => (
           <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
         ),
       }}
       {...props}
